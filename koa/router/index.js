6557c67ebe8bac840ad75d0688147a559c295e9c
wxy <657018487@qq.com>
2018-12-21 11:40:46 +0800

    添加private。key 加密

--- koa/router/index.js
+++ koa/router/index.js
@@ -1,6 +1,7 @@
 const router = require('koa-router')()
-const fs = require('fs')
 const jwt = require('jsonwebtoken')
+const fs = require('fs')
+let privateKey = fs.readFileSync('./jsonwebtoken/config/private.key')
 // Token 数据
 const payload = {
   name: 'wanghao',
@@ -20,7 +21,7 @@ console.log(token)
 const test = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoid2FuZ2hhbyIsImFkbWluIjp0cnVlLCJpYXQiOjE1NDUyNzYxNTksImV4cCI6MTU0NTI3OTc1OX0.pZScZXnc2FSJMrtG2NhWnsBi9OFPbtd7MJif6Z0Iguk`
 // const test = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoid2FuZ2hhbyIsImFkbWluIjp0cnVlLCJpYXQiOjE1NDUyODk2ODUsImV4cCI6MTU0NTI4OTk4NX0.GZplMImI8aSPhRUUcoXoAYV46oHNzVuRGvKKVwLcMc0`
 // 到达结束时间后会过期返回error
-jwt.verify(test, secret, (error, decoded) => {
+jwt.verify(test, privateKey, (error, decoded) => {
   if (error) {
     console.error(error.message)
   } else {
@@ -32,6 +33,21 @@ router.get('/', async (ctx, next) => {
   await ctx.render('index', {
     title: 'Hello Koa 2!'
   })
+})
+
+router.get('/about', async (ctx, next) => {
+  ctx.body = {
+	code: 0,
+	msg: 'about'
+  }
+})
+
+
+router.get('/hello', async (ctx, next) => {
+  ctx.body = {
+	code: 0,
+	msg: 'hello'
+  }
 })
 
 router.get('/token', async (ctx, next) => {
@@ -48,10 +64,13 @@ router.get('/token', async (ctx, next) => {
     }
       return
   }
-  jwt.verify(authorization, secret, (error, decoded) => {
+  jwt.verify(authorization, privateKey, (error, decoded) => {
     if (error) {
       console.log(error.message)
-      ctx.body = error.message
+      ctx.body = {
+		  code: "-2",
+		  msg: error.message
+	  }
       return
     }
     console.log(decoded)
@@ -79,7 +98,7 @@ router.post('/token', async (ctx, next) => {
             let userToken = {
                 name: user.name
             }
-            const token = jwt.sign(userToken, secret, {expiresIn: '1h'})  //token签名 有效期为1小时
+            const token = jwt.sign(userToken, privateKey, {expiresIn: '1h'})  //token签名 有效期为1小时
             ctx.body = {
                 message: '获取token成功',
                 code: 1,
